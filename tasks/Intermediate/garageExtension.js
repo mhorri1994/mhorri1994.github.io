0739fc3899f6cf74e16952d7fc1b44680022b514
QACmhorrigan <matthew.horrigan@qa.com>
2017-08-24 12:59:22 +0100

    Updated garageExtension functionality

--- tasks/Intermediate/garageExtension.js
+++ tasks/Intermediate/garageExtension.js
@@ -1,9 +1,28 @@
+let inputHistory = [];
+let position = 0;
 function checkKey(event) {
+    let textField = document.getElementById('command');
     if (event.key === 'Enter') {
-        let textField = document.getElementById('command');
         runCommand(textField.value);
+        inputHistory.push(textField.value);
         textField.value = '';
+        position = inputHistory.length;
     }
+    if(event.key === 'ArrowUp'){
+        textField.value = inputHistory[position];
+        position -= 1;
+        if(position === -1) {
+            position = 0;
+        }
+    }
+    if(event.key === 'ArrowDown'){
+        textField.value = inputHistory[position];
+        position += 1;
+        if(position === inputHistory.length + 1) {
+            position = inputHistory.length;
+        }
+    }
+    console.log(event.key);
 }
 
 function runCommand(command) {
@@ -25,6 +44,15 @@ function runCommand(command) {
             }
             break;
         case 'check':
+            if(command[1] === 'in'){
+                checkIn(command[2], 'console');
+            }
+            if(command[1] === 'out'){
+                checkOut(command[2], 'console');
+            }
+            break;
+        case 'remove':
+            removeVehicleByReg(command[1]);
             break;
     }
 }
